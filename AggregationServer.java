23cf7d8f87f047dfca453e0151f18d8ea9e9d868
Shaan Sadanand <cheenu@Shaans-MacBook-Air.local>
2023-09-07 10:59:43 +0930

    Implement: basic aggregation sever with msg

--- AggregationServer.java
+++ AggregationServer.java
@@ -2,18 +2,53 @@ import java.net.*;
 import java.io.*;
 
 public class AggregationServer {
-    public static void main(String[] params) {
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        ServerSocket serverSocket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        String serverAddress = "localhost";
         int port = 9999;
 
         System.out.println("Starting aggregation server on port: " + port);
 
-        try (ServerSocket aggregationServerSocket = new ServerSocket(9999)) {
+        try (serverSocket = new ServerSocket(port)) {
             
             while (true) {
 
                 try {
 
-                    Socket clientSocket = aggregationServerSocket.accept();
+                    socket = serverSocket.accept();
+
+                    inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                    bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+                    while (true) {
+
+                        String msg = bufferedReader.readLine();
+
+                        System.out.println("Client: " + msg);
+
+                        bufferedWriter.write("MSG received!")
+                        bufferedWriter.newLine();
+                        bufferedWriter.flush();
+
+                        if (msg.equalsIgnoreCase("BYE")){
+                            break;
+                        }
+                    }
+
+                    socket.close();
+                    inputStreamReader.close();
+                    outputStreamWriter.close();
+                    bufferedReader.close();
+                    bufferedWriter.close();
 
                 } catch (IOException e) {
                     System.out.println("Aggregation server socket was closed...");
