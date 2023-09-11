92e7211592fec47a0d8882c2f0699237b707eaac
Shaan Sadanand <cheenu@Shaans-MacBook-Air.local>
2023-09-12 02:21:00 +0930

    Implement: converting content to object and printing

--- ContentServer.java
+++ ContentServer.java
@@ -43,37 +43,71 @@ public class ContentServer {
     public static WeatherObject buildWeatherObject(String input) {
 
         String[] lines = input.split("\n");
+        WeatherObject weather = new WeatherObject();
 
         for (String line : lines) {
-            
+
             String[] objectParts = line.split(":");
 
             String parameter = objectParts[0].trim();
             String value = objectParts[1].trim();
 
+            switch (parameter) {
+                case "id":
+                    weather.setId(value);
+                    break;
+                case "name":
+                    weather.setName(value);
+                    break;
+                case "state":
+                    weather.setState(value);
+                    break;
+                case "time_zone":
+                    weather.setTime_zone(value);
+                    break;
+                case "lat":
+                    weather.setLat(Float.parseFloat(value));
+                    break;
+                case "lon":
+                    weather.setLon(Float.parseFloat(value));
+                    break;
+                case "local_date_time":
+                    weather.setLocal_date_time(value);
+                    break;
+                case "local_date_time_full":
+                    weather.setLocal_date_time_full(value);
+                    break;
+                case "air_temp":
+                    weather.setAir_temp(Float.parseFloat(value));
+                    break;
+                case "apparent_t":
+                    weather.setApparent_t(Float.parseFloat(value));
+                    break;
+                case "cloud":
+                    weather.setCloud(value);
+                    break;
+                case "dewpt":
+                    weather.setDewpt(Float.parseFloat(value));
+                    break;
+                case "press":
+                    weather.setPress(Float.parseFloat(value));
+                    break;
+                case "rel_hum":
+                    weather.setRel_hum(Integer.parseInt(value));
+                    break;
+                case "wind_dir":
+                    weather.setWind_dir(value);
+                    break;
+                case "wind_spd_kmh":
+                    weather.setWind_spd_kmh(Integer.parseInt(value));
+                    break;
+                case "wind_spd_kt":
+                    weather.setWind_spd_kt(Integer.parseInt(value));
+                    break;
+                default:
+                    break;
+            }
         }
-
-
-
-        WeatherObject weather = new WeatherObject();
-        weather.setId("IDS60901");
-        weather.setName("Adelaide (West Terrace / ngayirdapira)");
-        weather.setState("SA");
-        weather.setTime_zone("CST");
-        weather.setLat(-34.9);
-        weather.setLon(138.6);
-        weather.setLocal_date_time("15/04:00pm");
-        weather.setLocal_date_time_full("20230715160000");
-        weather.setAir_temp(13.3);
-        weather.setApparent_t(9.5);
-        weather.setCloud("Partly cloudy");
-        weather.setDewpt(5.7);
-        weather.setPress(1023.9);
-        weather.setRel_hum(60);
-        weather.setWind_dir("S");
-        weather.setWind_spd_kmh(15);
-        weather.setWind_spd_kt(8);
-
         return weather;
     }
 
@@ -111,7 +145,8 @@ public class ContentServer {
         System.out.println(content);
 
         // Build objects for each entry in content
-
+        WeatherObject weather1 = buildWeatherObject(content);
+        System.out.println(weather1);
 
         // Parse string to json
         //Gson gson = new Gson();
