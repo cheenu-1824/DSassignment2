5742f0e54298177eac2534ee9c3fc9f7a16644b1
Shaan Sadanand <cheenu@Shaans-MacBook-Air.local>
2023-09-25 03:48:13 +0930

    Implement: Client threading class

---
+++ ClientThread.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientThread {
+
+    private Socket socket;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void start() {
+
+        int maxClients = 5;
+
+        try {
+
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            while (true) {
+
+                String msg = bufferedReader.readLine();
+
+                if (msg.equalsIgnoreCase("BYE")){
+                    bufferedWriter.write("BYE!");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                    break;
+                }
+
+                // Handle requests "GET", "PUT"
+                AggregationServer.handleReq(bufferedReader, bufferedWriter, msg);
+
+            }
+
+            for (int i = 0; i < maxClients; i++) {
+                if (AggregationServer.threads[i] == this) {
+                    AggregationServer.threads[i] = null;
+                    break;
+                }
+            }
+
+            socket.close();
+            inputStreamReader.close();
+            outputStreamWriter.close();
+            bufferedReader.close();
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: Failed to start threaded client socket...");
+        }
+    }
+
+}
